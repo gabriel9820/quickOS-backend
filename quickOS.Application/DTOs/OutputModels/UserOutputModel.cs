ba38e51a518b517537e663653585907b5cb36a0d
Gabriel Alves <gabriel_alves_44@live.com>
2024-03-16 11:26:30 -0300

    refactor: return role useroutputmodel

--- quickOS.Application/DTOs/OutputModels/UserOutputModel.cs
+++ quickOS.Application/DTOs/OutputModels/UserOutputModel.cs
@@ -1,15 +1,19 @@
-﻿namespace quickOS.Application.DTOs.OutputModels;
+﻿using quickOS.Core.Enums;
+
+namespace quickOS.Application.DTOs.OutputModels;
 
 public class UserOutputModel
 {
-    public Guid ExternaId { get; private set; }
+    public Guid ExternalId { get; private set; }
     public string FullName { get; private set; }
     public string Email { get; private set; }
+    public Role Role { get; private set; }
 
-    public UserOutputModel(Guid externalId, string fullName, string email)
+    public UserOutputModel(Guid externalId, string fullName, string email, Role role)
     {
-        ExternaId = externalId;
+        ExternalId = externalId;
         FullName = fullName;
         Email = email;
+        Role = role;
     }
 }
