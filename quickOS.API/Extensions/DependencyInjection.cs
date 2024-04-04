793c36969cc9abc795c26f72ea748dd8fee50aea
Gabriel Alves <gabriel_alves_44@live.com>
2024-04-03 21:46:54 -0300

    feat: env test

--- quickOS.API/Extensions/DependencyInjection.cs
+++ quickOS.API/Extensions/DependencyInjection.cs
@@ -100,6 +100,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         Console.WriteLine(configuration["TESTE"]);
+        Console.WriteLine(configuration["DATABASE_NAME"]);
         services.AddDbContext<QuickOSDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("quickOS")));
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<ITokenService, TokenService>();
