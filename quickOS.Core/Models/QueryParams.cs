7cdcb427f2881e4f3c0fe59680c6f0d5a0b71c2c
Gabriel Alves <gabriel_alves_44@live.com>
2024-05-07 20:14:26 -0300

    feat: page size

--- quickOS.Core/Models/QueryParams.cs
+++ quickOS.Core/Models/QueryParams.cs
@@ -7,7 +7,7 @@ public abstract class QueryParams
     [Range(1, int.MaxValue, ErrorMessage = "O campo {0} deve ser um valor entre {1} e {2}")]
     public int CurrentPage { get; set; } = 1;
 
-    [Range(5, 50, ErrorMessage = "O campo {0} deve ser um valor entre {1} e {2}")]
+    [Range(5, 100, ErrorMessage = "O campo {0} deve ser um valor entre {1} e {2}")]
     public int PageSize { get; set; } = 10;
 
     public string OrderBy { get; set; } = string.Empty;
