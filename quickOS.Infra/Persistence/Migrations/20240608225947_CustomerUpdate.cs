d3a20fcffc8af14ad40f6003f1c73c8a452789a2
Gabriel Alves <gabriel_alves_44@live.com>
2024-06-08 20:00:27 -0300

    feat: customers migration

---
+++ quickOS.Infra/Persistence/Migrations/20240608225947_CustomerUpdate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace quickOS.Infra.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomerUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Code",
+                table: "Customers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Code_TenantId",
+                table: "Customers",
+                columns: new[] { "Code", "TenantId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_Code_TenantId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Customers");
+        }
+    }
+}
