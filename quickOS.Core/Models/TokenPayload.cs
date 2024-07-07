e6a4e341d314d7675f9c96df186fe11faabf2a00
Gabriel Alves <gabriel_alves_44@live.com>
2024-07-07 20:19:03 -0300

    feat: get user role in request provider

--- quickOS.Core/Models/TokenPayload.cs
+++ quickOS.Core/Models/TokenPayload.cs
@@ -1,15 +1,19 @@
-﻿namespace quickOS.Core.Models;
+﻿using quickOS.Core.Enums;
+
+namespace quickOS.Core.Models;
 
 public class TokenPayload
 {
     public int UserId { get; private set; }
     public string UserEmail { get; private set; }
+    public UserRole UserRole { get; private set; }
     public int TenantId { get; private set; }
 
-    public TokenPayload(string userId, string userEmail, string tenantId)
+    public TokenPayload(string userId, string userEmail, string tenantId, UserRole userRole)
     {
         UserId = int.Parse(userId);
         UserEmail = userEmail;
         TenantId = int.Parse(tenantId);
+        UserRole = userRole;
     }
 }
